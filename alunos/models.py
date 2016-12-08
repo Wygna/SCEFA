c6d70c6ee46923797e90673dfb7152254c93360b
wallysson <wallyssonlds@gmail.com>
2016-12-08 18:03:05 -0300

    update models and urls

--- alunos/models.py
+++ alunos/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Aluno(models.Model):
     nome = models.CharField("Nome", max_length=200)
     email = models.CharField("e-mail", max_length=200)
+    turno_aula = models.CharField("Turno", max_length=20)
     matricula = models.CharField("Matrincula", max_length=14, unique=True)
 
     def __str__(self):
         return self.nome
 
 
+#class Administrador(models.Model):
+#    nome = models.CharField("Nome", max_length=200)
+#    endereco = models.CharField("Endereco", max_length=255)
--- alunos/urls.py
+++ alunos/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     #url(r'^$', home, name='home')
+    url(r'^$')
 ]
