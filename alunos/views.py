20e4735bd6c6f2e723b6c28cf1d6ede971177a1a
wallysson <wallyssonlds@gmail.com>
2016-12-10 14:06:27 -0300

    update views and add migrations

--- alunos/views.py
+++ alunos/views.py
@@ -1,5 +1,18 @@
+from django.conf.locale import id
 from django.shortcuts import render, redirect
 from django.db.models.fields import Empty
 
-def home(request):
-    return render('base.html')
+
+from alunos.froms import AlunoForm
+
+
+def aluno_new(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('alunos_list')
+    else:
+        form = AlunoForm
+        dados = {'form': form}
+        return render(request, 'Alunos/aluno_form.html', dados)
