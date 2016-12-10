92b3aadfc1d44021aad5695584870e0ba1d60247
wallysson <wallyssonlds@gmail.com>
2016-12-10 15:31:24 -0300

    update views and list templates

--- alunos/urls.py
+++ alunos/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from alunos.views import alunos_new
+from alunos.views import *
 
 urlpatterns = [
-    url(r'^alunos/new/$', alunos_new, name='alunos_new')
+    url(r'^alunos/list$', aluno_list, name='aluno_list'),
+    url(r'^alunos/new/$', aluno_new, name='alunos_new'),
+    url(r'^alunos/detail/(?P<pk>\d+)$', aluno_detail, name='alunos_detail'),
 ]
--- alunos/views.py
+++ alunos/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from django.db.models.fields import Empty
 
 
-from alunos.froms import AlunoForm
+from alunos.forms import AlunoForm
+from alunos.models import Aluno
 
 
 def aluno_new(request):
@@ -16,3 +17,18 @@ def aluno_new(request):
         form = AlunoForm
         dados = {'form': form}
         return render(request, 'Alunos/aluno_form.html', dados)
+
+
+def aluno_detail(request, pk):
+    aluno = Aluno.objects.get(id=pk)
+    return render(request, 'Alunos/exibirAlunos.html')
+
+
+def aluno_list(request):
+    criterio = request.GET.get('criterio')
+    if criterio:
+        alunos = Aluno.objects.filter(nome__contains=criterio).order_by('nome')
+    else:
+        alunos = Funcionario.objects.all().order_by('nome')
+        criterio = ''
+    return render(request, 'Alunos/alunos_list.html', dados)
