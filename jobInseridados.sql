9919f46917e96f56dd39ba6b79386098d96172be
ednilsonpalhares <ednilsonpalhares@hotmail.com>
2017-06-15 20:59:53 -0300

    criação de grupo automatico

---
+++ jobInseridados.sql
@@ -0,0 +1,4 @@
+insert into public."appPonto_frequencia"(data, pessoa_id)
+	select current_date,f.pessoa_ptr_id from public."appPonto_funcionario" f, public."appPonto_dias_sem_expediente" d
+	where f.pessoa_ptr_id not in (select fr.pessoa_id from public."appPonto_frequencia" fr where fr.data =  current_date)
+	and d.data != current_date;
