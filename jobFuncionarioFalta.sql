52e564a658fbf64e9e2edbba0122d568b7fb7768
Ednilson Palhares <ednilsonpalhares@hotmail.com>
2017-06-15 20:36:38 -0300

    Delete jobFuncionarioFalta.sql

--- jobFuncionarioFalta.sql
+++
@@ -1,3 +0,0 @@
-insert into public."appPonto_frequencia"(data, pessoa_id)
-	select current_date,f.pessoa_ptr_id from public."appPonto_funcionario" f 
-	where f.pessoa_ptr_id not in (select fr.pessoa_id from public."appPonto_frequencia" fr where fr.data =  current_date)
