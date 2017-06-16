23351a0220659e6b33e27b790766a16298589500
ednilsonpalhares <ednilsonpalhares@hotmail.com>
2017-06-16 08:40:47 -0300

    Correção de update de Administrador

--- ProjetoPontoPython/settings.py
+++ ProjetoPontoPython/settings.py
@@ -84,14 +84,7 @@ DATABASES = {
     #   'ENGINE': 'django.db.backends.sqlite3',
     #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BancoPonto',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',  # 8000 is default8
-    }
+
     # 'default': {
     #   'ENGINE': 'django.db.backends.mysql',
     #  'NAME': 'bancoponto',
