c7e2130621cf1af7935895a07745a11af69ab0e0
Ednilson Palhares <ednilson@ip-172-31-24-166.us-west-2.compute.internal>
2017-07-25 14:34:51 +0000

    correção

--- ProjetoPontoPython/settings.py
+++ ProjetoPontoPython/settings.py
@@ -83,6 +83,15 @@ DATABASES = {
     # 'default': {
     #   'ENGINE': 'django.db.backends.sqlite3',
     #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bancoponto',
+        # 'NAME': os.path.join(BASE_DIR, 'mydb'),
+        'USER': 'postgres2',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432', # 8000 is default
+    }
 
 }
 
--- appPortas/consultas.py
+++ appPortas/consultas.py
@@ -4,24 +4,25 @@ from appPonto.models import *
 from appPortas.models import *
 from datetime import datetime
 
-def conultar_pessoa_acesso_porta(id_digital, id_porta, img_digital):
+
+def consultar_pessoa_acesso_porta(id_digital, id_porta, img_digital):
     try:
-        pessoagrupo = GrupoPessoa.objects.filter(pessoa__id_digital=id_digital)
-        portagrupo = GrupoPorta.objects.filter(porta_id=id_porta)
+        grupopessoa = GrupoPessoa.objects.filter(pessoa__id_digital=id_digital)
+        grupoporta = GrupoPorta.objects.filter(porta_id=id_porta)
     except GrupoPessoa.DoesNotExist:
         return False
     except GrupoPorta.DoesNotExist:
         return False
     acesso = False
-    for pessoa in pessoagrupo:
-        for porta in portagrupo:
-            if pessoa.id == porta.id:
+    for grupo in grupopessoa:
+        for porta in grupoporta:
+            if grupo.grupo == porta.grupo:
                 acesso = True
     return acesso
 
 def registra_acesso_porta(id_digital, id_porta, img_digital):
     hora = datetime.now()
-    if conultar_pessoa_acesso_porta(id_digital, id_porta) == True:
+    if consultar_pessoa_acesso_porta(id_digital, id_porta, img_digital) == True:
         pessoa = Pessoa.objects.get(id_digital=id_digital)
         porta = Porta.objects.get(id=id_porta)
         registro_acesso = RegistroPorta.objects.create(data=datetime.now().date(),
@@ -33,15 +34,25 @@ def registra_acesso_porta(id_digital, id_porta, img_digital):
         return False
 
 def registra_frequencia_entrada(id_digital, img_digital, local):
-    hora = datetime.now()
+    not_frequencia = True
     try:
-        pessoa = Pessoa.objects.get(id_digital=id_digital)
-    except Pessoa.DoesNotExist:
+        Frequencia.objects.get(pessoa__id_digital=32, data=datetime.now().date())
+        not_frequencia = False
+    except Frequencia.DoesNotExist:
+        not_frequencia = True
+    hora = datetime.now()
+
+    if not_frequencia:
+        try:
+            pessoa = Pessoa.objects.get(id_digital=id_digital)
+        except Pessoa.DoesNotExist:
+            return False
+        frequencia = Frequencia.objects.create(data=datetime.now().date(), hora_entrada=hora.strftime("%H:%M:%S"),
+                                               hora_saida=None, pessoa=pessoa, local=local)
+        frequencia.save()
+        return True
+    else:
         return False
-    frequencia = Frequencia.objects.create(data=datetime.now().date(), hora_entrada=hora.strftime("%H:%M:%S"),
-                                           hora_saida=None, pessoa=pessoa, local=local)
-    frequencia.save()
-    return True
 
 def registra_frequencia_saida(id_digital, img_digital, local):
     hora = datetime.now()
@@ -54,7 +65,6 @@ def registra_frequencia_saida(id_digital, img_digital, local):
     frequencia.save()
     return True
 
-
 def adicionar_biometria(id_digital, img_digital, matricula):
     try:
         pessoa = Pessoa.objects.get(username=matricula)
@@ -66,4 +76,4 @@ def adicionar_biometria(id_digital, img_digital, matricula):
     return True
 
 
-print(conultar_pessoa_acesso_porta(2, 2, "oo"))
+print(adicionar_biometria(88, 'img', 251))
